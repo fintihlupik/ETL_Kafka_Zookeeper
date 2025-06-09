b4a92d0ff206defd9eb55e0750ca05ab635c3c62
fintihlupik <aminaalive@gmail.com>
2025-06-09 12:11:42 +0200

    fix: add logic to avoid duplicates in mongo collections insertion

--- kafka_consumer/storage_mongo.py
+++ kafka_consumer/storage_mongo.py
@@ -1,28 +1,35 @@
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://mongo:27017/")
-db = client["raw_mongo_db"]  # Nombre de la base de datos
+db = client["raw_mongo_db"]
 
-# Guardar el mensaje crudo en MongoDB
 def guardar_en_mongo(documento):
     try:
         keys = set(k.lower() for k in documento.keys())
 
         if "passport" in keys and "name" in keys:
             collection = db["personal_data"]
+            filter_key = {"passport": documento.get("passport")}
         elif "fullname" in keys and "city" in keys:
             collection = db["location_data"]
+            filter_key = {"fullname": documento.get("fullname")}
         elif "company" in keys or "job" in keys:
             collection = db["professional_data"]
+            filter_key = {"fullname": documento.get("fullname")}
         elif "iban" in keys:
             collection = db["bank_data"]
+            filter_key = {"passport": documento.get("passport")}
         elif "ipv4" in keys:
             collection = db["net_data"]
+            # usar IPv4 como filtro único
+            filter_key = {"IPv4": documento.get("IPv4")}
         else:
             collection = db["unknown_type"]
+            filter_key = documento  # sin filtro único, podría cambiar
 
-        collection.insert_one(documento)
-        print(f"✅ Documento guardado en colección: {collection.name}")
+        collection.update_one(filter_key, {"$set": documento}, upsert=True)
+        print(f"✅ Documento guardado o actualizado en colección: {collection.name}")
 
     except Exception as e:
         print("❌ Error al guardar en MongoDB:", e)
+
